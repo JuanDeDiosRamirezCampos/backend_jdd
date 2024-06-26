91888c3d64cffa1ca01ae75ded5952cd44a40288
JuanDeDiosRamirezCampos <juanmatacuicos@gmail.com>
2024-06-26 13:51:47 -0600

    jdd Manejo de rutas

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,42 @@
-import express from 'express';
-const app = express();
+import express, {Application} from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import authRoutes   from './routes/authRoutes';
+/*
+Clase de inicio de nuestra aplicación NodeJsExpress
+Autor: Juan de Dios Ramirez Campos
+Fecha: 21 Junio 2023
+*/
+class Server {
+  private app: Application;
+//Iniciatiza clase
+constructor() {
+  this.app = express();
+  this.config();
+  this.routes();
+  this.app.listen(this.app.get("port"), () => {
+    console.log("Server on port", this.app.get("port"));
+});      
+}
+//Configuración de modulos
+config(): void {
+  // configuración del puerto para el servidor
+  this.app.set("port", 3000);
  
-/*  DotEnv  */
-const dotenv = require('dotenv');
-dotenv.config();
-const port = process.env.PORT;
- 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
- 
-app.listen(port, () => {
-  return console.log(`Express is listening at http://localhost:${port}`);
-});
+  // muestra las peticiones en consola
+  this.app.use(morgan("dev"));
+
+  // puertos de conexión de la API
+  this.app.use(cors());
+
+  // solo se permiten peticiones en formato JSON
+  this.app.use(bodyParser.json());
+  this.app.use(bodyParser.urlencoded({extended: false,}))
+  }
+//Configura Las rutas
+routes() {
+  this.app.use("/",authRoutes)
+}
+}
+const server = new Server();
